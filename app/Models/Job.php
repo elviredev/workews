8444c270ef93164fb33fe85dd4a09f7adbdc9a3e
elviredev <sandrine.rodriguez@hotmail.com>
2024-10-05 18:53:44 +0200

    ✨Créer relation one-to-many entre les tables "users" et "job_listings"

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,12 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $table = 'job_listings';
+  protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'description'];
+  protected $fillable = [
+    'title',
+    'description',
+    'salary',
+    'tags',
+    'job_type',
+    'remote',
+    'requirements',
+    'benefits',
+    'address',
+    'city',
+    'state',
+    'zipcode',
+    'contact_email',
+    'contact_phone',
+    'company_name',
+    'company_description',
+    'company_logo',
+    'company_website',
+    'user_id'
+  ];
+
+  // Relation avec le user
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
 }
